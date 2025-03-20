4d88bb6cfd41f0cab337faf5240199e44d6b4599
RafliNST <rafli.ran3103@gmail.com>
2025-03-20 20:59:36 +0700

    add styles

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ function loadPage($page){
   if ($page === 'home') include 'file_/home.php';
   else if ($page === 'login') include 'file_/login.php';
   else if ($page === 'logout') include 'file_/logout.php';
+  else if ($page === 'signup') include 'file_/signup.php';
   else if ($page === 'admin_dashboard' 
     && isset($_SESSION['role_id']) 
     && $_SESSION['role_id'] == 1) include 'file_/admin_dashboard.php';    
